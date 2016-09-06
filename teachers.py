0a1e866900bd7423f7ddb052840a43f98f6565f2
Dylan Cascio <Dcascio73@gmail.com>
2016-09-05 22:14:23 -0700

    Forgot to add the teachers data from the end of the Dictionaries section.

--- teachers.py
+++ teachers.py
@@ -1,9 +1,10 @@
-# Create a function named most_classes that takes a dictionary of teachers.
-# Each key is a teacher's name and their value is a list of classes they've taught.
-# most_classes should return the teacher with the most classes.
-# 
+# Create a function named most_classes that takes a dictionary of teachers
+# Each key is a teacher's name and their value is a list of classes they've
+# taught. most_classes should return the teacher with the most classes.
+#
 # The dictionary will be something like:
-# {'Jason Seifer': ['Ruby Foundations', 'Ruby on Rails Forms', 'Technology Foundations'],
+# {'Jason Seifer': ['Ruby Foundations', 'Ruby on Rails Forms',
+#                   'Technology Foundations'],
 #  'Kenneth Love': ['Python Basics', 'Python Collections']}
 #
 # Often, it's a good idea to hold onto a max_count variable.
@@ -13,10 +14,14 @@
 #
 # Your code goes below here.
 
-my_dict = {'Jason Seifer': ['Ruby Foundations', 'Ruby on Rails Forms', 'Technology Foundations'],
+my_dict = {'Jason Seifer': ['Ruby Foundations', 'Ruby on Rails Forms',
+                            'Technology Foundations'],
            'Kenneth Love': ['Python Basics', 'Python Collections'],
-           'Dylan Cascio': ['Pythong 1', 'Chrome Dev Tools', 'Getting Started with Squarespace',
-                            'Another Class of Sorts']}
+           'Dylan Cascio':
+           ['Pythong 1', 'Chrome Dev Tools',
+            'Getting Started with Squarespace', 'Another Class of Sorts']}
+
+
 def most_classes(dict1):
     max_count = 0
     name = ''
@@ -28,7 +33,8 @@ def most_classes(dict1):
                 if values == value:
                     name = key
     return name
-    
+
+
 print(most_classes(my_dict))
 
 print("-----------")
@@ -36,20 +42,22 @@ print("-----------")
 # Next, create a function named num_teachers that takes the same dictionary
 # of teachers and classes. Return the total number of teachers.
 
+
 def num_teachers(dict1):
     teachers = 0
     for teacher in dict1.keys():
         teachers += 1
     return teachers
 
+
 print(num_teachers(my_dict))
 
 print("-----------")
 
-
 # Now, create a function named stats that takes the teacher dictionary.
 # Return a list of lists in the format [<teacher name>, <number of classes>].
 # For example, one item in the list would be ['Dave McFarland', 1].
+
 
 def stats(dict1):
     teachers = []
@@ -57,12 +65,15 @@ def stats(dict1):
         value = len(value)
         teachers.append([key, value])
     return teachers
-    
+
+
 print(stats(my_dict))
 print("-----------")
 
-# Great work! Finally, write a function named courses that takes the teachers dictionary.
-# It should return a single list of all of the courses offered by all of the teachers.
+# Great work! Finally, write a function named courses that takes the teachers
+# dictionary. It should return a single list of all of the courses offered by
+# all of the teachers.
+
 
 def courses(dict1):
     course_list = []
@@ -70,6 +81,8 @@ def courses(dict1):
         course_list += course
     return course_list
 
+
 print(courses(my_dict))
-    
+
 print("-----------")
+print(70 + 2.5)
