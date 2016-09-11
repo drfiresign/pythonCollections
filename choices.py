796604e53f9f6fd92df5af04c2a18ea897d9ab15
Dylan Cascio <Dcascio73@gmail.com>
2016-09-11 14:25:54 -0700

    Updated choices.py to use correct syntax
    I appended incorrectly. Fixed this error.

--- choices.py
+++ choices.py
@@ -4,5 +4,10 @@ from random import choice
 def nchoices(iterable, n):
     output = []
     while n > 0:
-        choice(iterable).append(output)
+        output.append(choice(iterable))
         n -= 1
+    return output
+
+
+things = list(range(50))
+print(nchoices(things, 50))
